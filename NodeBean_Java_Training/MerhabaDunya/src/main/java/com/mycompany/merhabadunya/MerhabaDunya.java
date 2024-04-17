8507e2daf72f3905a747f2645a84a9261b40c83c
Utku Doğanay <124122447+utkudoganay9@users.noreply.github.com>
2024-04-17 13:31:06 +0300

    Update MerhabaDunya.java

--- NodeBean_Java_Training/MerhabaDunya/src/main/java/com/mycompany/merhabadunya/MerhabaDunya.java
+++ NodeBean_Java_Training/MerhabaDunya/src/main/java/com/mycompany/merhabadunya/MerhabaDunya.java
@@ -11,16 +11,14 @@ package com.mycompany.merhabadunya;
 public class MerhabaDunya1 {
 
     public static void main(String[] args) {
-        // bu cümlenin ilk ve son kelimelerini yazdır
-        // cümle : ben yazılım ögrenmeye calısıyorum
-        String cumle = "ben yazılım ögrenmeye calısıyorum";
+        
+        String cumle = "bugün hava çok güzel";
         int cumleilkbosluk = cumle.indexOf(' ');
         int cumlesonbosluk = cumle.lastIndexOf(' ');
         System.out.println("cumleilkbosluk: " + cumleilkbosluk + "cumlesonbosluk: " +  cumlesonbosluk);
         String ilkkelime = cumle.substring(0 ,cumleilkbosluk );
         String sonkelime = cumle.substring(cumlesonbosluk+1);
         System.out.println("ilkkelime: " + ilkkelime +"sonkelime: " + sonkelime);
-        // yukarıda belirtilen cümledeki ilk kelimenin 1. ve son kelimenin 2.harfini yazdır
         System.out.println("ilkkelime: " + ilkkelime.charAt(0) + " sonkelime: " + sonkelime.charAt(1));
        
         
