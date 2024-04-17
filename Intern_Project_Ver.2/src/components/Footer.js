96aabfe8455ae4c141dc62a018130c0a6f099b3b
Utku Doğanay <124122447+utkudoganay9@users.noreply.github.com>
2024-04-17 13:21:58 +0300

    Update Footer.js

--- Intern_Project_Ver.2/src/components/Footer.js
+++ Intern_Project_Ver.2/src/components/Footer.js
@@ -7,7 +7,7 @@ function Footer() {
   return (
     <footer className=" w-auto right-0">
       <div className="flex justify-end">
-      <img src={process.env.PUBLIC_URL + '/img/okinar.svg'} alt="logo" className="w-auto h-50" />
+      <img src={process.env.PUBLIC_URL + '/img/'} alt="logo" className="w-auto h-50" />
         </div>
         <div className="text-left text-gray-800"style={{ marginTop: "-38px" }}><p>Copyright &copy; 2023 </p>Version 3.6</div>
     </footer>
