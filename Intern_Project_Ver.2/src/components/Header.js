12a884fe095bbd2756c611b9f82ca28879212895
Utku Doğanay <124122447+utkudoganay9@users.noreply.github.com>
2024-04-17 13:22:21 +0300

    Update Header.js

--- Intern_Project_Ver.2/src/components/Header.js
+++ Intern_Project_Ver.2/src/components/Header.js
@@ -2,7 +2,7 @@ import React, { useState } from 'react';
 import { FaBell } from 'react-icons/fa';
 import { IoExit } from 'react-icons/io5';
 import { useNavigate } from 'react-router-dom';
-import "./css/Header.css" // Header.css dosyasını ekleyin
+import "./css/Header.css"
 
 const Header = () => {
     const [isBellDropdownOpen, setBellDropdownOpen] = useState(false);
@@ -57,13 +57,12 @@ const Header = () => {
   
         {isBellDropdownOpen && (
           <div className="absolute top-14 right-0 bg-white border rounded shadow p-4">
-            {/* Bell Dropdown içeriği */}
+            
             <p>Bildiriminiz bulunmamaktadır.</p>
            
           </div>
         )}
   
-        {/* Exit dropdown içeriği burada gösterilmiyor */}
       </div>
     );
   };
